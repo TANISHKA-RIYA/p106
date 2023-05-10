9179578285e485c1ca8182113a0a6ba7e5a2e053
TANISHKA-RIYA <106598143+TANISHKA-RIYA@users.noreply.github.com>
2023-05-10 23:19:06 +0530

    p106

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 //https://teachablemachine.withgoogle.com/models/CQVA3ZsOG/
+
+function startclassification(){
+    navigator.MediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/CQVA3ZsOG/model.json',modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
